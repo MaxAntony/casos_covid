267226d503a96cd4948c1f621ee72ca3f4c6382a
MaxAntony <maxcamito@gmail.com>
2020-04-16 03:55:23 -0500

    retirado bulma y agregado ng-bootstrap, chart, input pais y dia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from './services/data.service';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +8,42 @@ import { DataService } from './services/data.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'Covid-19';
+  // title = 'Covid-19';
   dataformated = [];
+  country = 'Peru';
+  // date
+  today: NgbDateStruct;
+
+  view: any[] = [600, 400];
+
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = false;
+  xAxisLabel = 'Casos';
+  showYAxisLabel = true;
+  yAxisLabel = 'Personas';
+
+  colorScheme = {
+    domain: ['#9370DB', '#87CEFA', '#FA8072', '#FF7F50', '#90EE90', '#9370DB'],
+  };
+
+  public single = [];
+
   constructor(private dataService: DataService) {}
+
   ngOnInit() {
+    this.getToday();
     this.getData();
+    // TODO: convertir el input de pais en desplegable y listar paises
+    // TODO: agregar funcionalidad de seleccionar fecha por rangos
   }
+
   getData() {
     this.dataService.getData().subscribe(
       (res) => {
-        this.dataformated = this.formatData(res as any[]);
+        this.formatData(res as any[]);
       },
       (err) => {
         console.log('ocurrio un error');
@@ -25,18 +52,70 @@ export class AppComponent implements OnInit {
   }
 
   formatData(data) {
-    const ciudad = [];
+    let dayStatus;
 
     for (const key in data) {
-      if (data.hasOwnProperty(key)) {
-        const cityName = key;
-        const deaths = data[key][data[key].length - 1].deaths;
-        const confirmed = data[key][data[key].length - 1].confirmed;
-        const recovered = data[key][data[key].length - 1].recovered;
-        ciudad.push({ cityName, deaths, confirmed, recovered });
+      if (
+        data.hasOwnProperty(key) &&
+        key.toLowerCase() === this.country.toLowerCase()
+      ) {
+        for (const day of data[key]) {
+          const date = day.date.split('-');
+          if (
+            Number(date[0]) === Number(this.today.year) &&
+            Number(date[1]) === Number(this.today.month) &&
+            Number(date[2]) === Number(this.today.day)
+          ) {
+            dayStatus = day;
+          }
+        }
       }
     }
 
-    return ciudad;
+    if (dayStatus) {
+      this.single = [
+        {
+          name: 'Muertos',
+          value: dayStatus?.deaths,
+        },
+        {
+          name: 'Infectados',
+          value: dayStatus?.confirmed,
+        },
+        {
+          name: 'Recuperados',
+          value: dayStatus?.recovered,
+        },
+      ];
+    } else {
+      this.single = [
+        {
+          name: 'No hay datos',
+          value: 0,
+        },
+      ];
+    }
+  }
+
+  onSelect(e) {
+    console.log(e);
+  }
+
+  getToday() {
+    const date = new Date();
+    this.today = {
+      year: Number(date.getFullYear()),
+      month: Number(date.getMonth()) + 1,
+      day: Number(date.getDate()) - 1,
+    };
+  }
+  countryChanged(e) {
+    this.getData();
+  }
+
+  dateChanged(e) {
+    if (this.today && this.today.year && this.today.month && this.today.day) {
+      this.getData();
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NgxChartsModule,
+    NgbModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
